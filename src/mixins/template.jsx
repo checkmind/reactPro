921c9d1965d2774414ee42e1f1c2b78c97d2bad6
checkmind <1036971959@qq.com>
2017-12-05 22:26:18 +0800

    ..

--- src/mixins/template.jsx
+++ src/mixins/template.jsx
@@ -2,7 +2,7 @@
 *  混合 connect到各个组件。
 *  如果想要调用store里面的数据必须要被connect
 */
-
+import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 const template = mySeting => {
@@ -12,5 +12,10 @@ const template = mySeting => {
         data: {}, //发送给服务器的数据
         component: <div></div>, //数据回调给的组件
     };
+    for(let key in mySeting){
+    	seting[key] = mySeting[key];
+    }
+    class parent extends Component {
 
+    }
 } 

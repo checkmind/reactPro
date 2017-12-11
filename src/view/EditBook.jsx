d745e39640fd658e3038ef7d0eec63edb69e0744
checkmind <1036971959@qq.com>
2017-12-11 23:57:31 +0800

    编辑页面

--- src/view/EditBook.jsx
+++ src/view/EditBook.jsx
@@ -16,22 +16,66 @@ class EditBook extends Component {
 	constructor(){
 		super();
 		this.state = {
-			"open": false
+			"open": false,
+			"words": [],
+			"title": '',
+			"content": ''
+		}
+
+		this.changeWords = function(){
+			let arr = this.state.words;
+			arr.push(this.state.title); //先放入标题
+			let str = ''
+			console.log(arr)
+			this.state.content.split('').forEach(function(val, index){
+				if(val!=' ')
+					str+= val;
+				else{
+					arr.push(str)
+					str = '';
+				}
+			})
+			arr.push(str)
+			console.log(arr)
+			this.setState({
+				words: arr
+			})
+		}
+		this.saveWords = ()=>{
+			this.changeWords();
+			console.log(this.state.words)
+		}
+		this.changTitle = (ev)=>{
+			this.setState({
+				title: ev.target.value
+			})
+			console.log(this.state)
+		}
+		this.changContent = (ev)=>{
+			this.setState({
+				content: ev.target.value
+			})
 		}
 	}	
 	render() {
+		let {title,content} = this.state;
 		return (
 		  <div className='editBook'>
 		  		<div className='header'>
 	              	<span className='down-arrow' onClick={()=>{ window.history.back()}}></span>
 		            <h3>新建竹简</h3>
-		            <a href='javascript:;' className='saveIcon' />
+		            <a href='javascript:;' className='saveIcon' onClick={this.saveWords} />
 		        </div>
 		        <div className='bodyer'>
 		        	<label>标题：</label>
-		        	<div contentEditable='true' className='editTitel' placeholder='请输入标题'></div>
+		        	<input type="text" 
+		        		   className='editTitel' 	
+		        		   onChange={this.changTitle} />
 		        	<label>内容：</label>
-		        	<div contentEditable='true' className='editContent'></div>
+		        	<input type='text'
+		        	   	   className='editContent' 
+		           	   	   onChange={this.changContent} />
+		        	
 		        </div>
 		  </div>
 		);
